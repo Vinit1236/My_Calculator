5f70f01f41227be0c69af31c7385f84e44f449e7
Vinit Pundeer <79745487+Vinit1236@users.noreply.github.com>
2022-12-18 23:14:06 +0530

    Updated

--- index.js
+++ index.js
@@ -1,28 +1,37 @@
 
 let screen = document.getElementById('screen');
 buttons = document.querySelectorAll('button');
-let screenValue = '';
-
+let screenValue = "";
 for (item of buttons) {
     item.addEventListener('click', (e) => {
         buttonText = e.target.innerText;
-        if (buttonText == 'X') {
+        if (buttonText == 'X' ) {
             buttonText = '*';
             screenValue += buttonText;
             screen.value = screenValue;
         }
-        else if (buttonText == 'AC') {
+        else if (buttonText == 'C') {
             screenValue = "";
             screen.value = screenValue;
         }
+        else if (buttonText == '←') {
+            screenValue = screenValue.substring(0,screenValue.length-1);
+            screen.value = screenValue;
+        }
         else if (buttonText == '=') {
-            screen.value = eval(screenValue);
+            try {
+              screen.value = eval(screenValue);
+              screenValue=screen.value;
+            } 
+            catch (error) {
+               screen.value="Syntax error!";
+               screenValue="";
+            }
         }
         else {
             screenValue += buttonText;
             screen.value = screenValue;
         }
-
     });
 }
 
